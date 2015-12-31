f9512be15bda7e9fabde557426545d231190c894
zpeng38 <zhenghongpeng@gatech.edu>
2015-12-31 17:12:15 -0600

    added bootstrap, angular, jqurrey, app.js

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module("CMapp", []);
+app.controller("CMcontroller",
+function ($scope) {
+$scope.hello = "Hello from App"
+});
---
+++ public/js/clientApp.js
@@ -0,0 +1,41 @@
+﻿var app = angular.module("DeveloperApp", []);
+
+app.controller("DeveloperController", function ($scope,$http) {
+    $scope.hello = " Angular Hello from DeveloperController";
+    $http.get("/developer")
+    .success(function (response) {
+        $scope.developers = response;
+    });
+
+    $scope.update = function (developer) {
+
+        //       $scope.developers[index] = developer;
+        $http.put("/developer/" + $scope.selectedindex, developer)
+        .success(function (response) {
+            $scope.developers = response;
+        });
+
+    };
+    $scope.selectedindex = null;
+    $scope.select = function (index) {
+
+        $scope.selectedindex = index;
+            $scope.developer = $scope.developers[index];
+        
+    };
+    $scope.remove = function (index) {
+        $http.delete("/developer/" + index)
+        .success(function (response) {
+            $scope.developers = response;
+        });
+    };
+    $scope.add = function (developer) {
+
+        //        console.log(developers);
+        $http.post("/developer",developer)
+            .success(function (response) {
+                $scope.developers = response;
+});
+    };
+    
+});
