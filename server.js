c28b824dbc028dbbbf79716b26f9de7c47ac1452
zpeng38 <zhenghongpeng@gatech.edu>
2015-12-31 16:48:32 -0600

    public

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 
+/*
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+*/
 
 
 var websites = [{ name: "site 1" }, { name: "site 2" }, { name: "site 3" }, { name: "site 4" }];
